3d371d7d4aa168f0a0752a62ce51105deb00c083
711chitti <711chitti@gmail.com>
2019-05-05 23:33:16 +0530

    first push

---
+++ src/app/model.ts
@@ -0,0 +1,49 @@
+export class Donate{
+        name:string;
+        mobile:any;
+        email:any;
+        password:any;
+        blood_group:string;
+        landline:string;
+        occupation:string;
+        country:string;
+        state:string;
+        district:string;
+        city:string;
+        address:string;
+        is_available:string;
+}
+export class Contact{
+        name:string;
+        mobile:string;
+        email:string;
+        subject:string;
+        message:string;
+}
+
+export class User{
+        name: string;
+        blood_group:string;
+        mobile:string;
+        landline: string;
+        email: any;
+        password: string;
+        token: any;
+        occupation:string;
+        country:string;
+        state: string;
+        district:string;
+        city: string;
+}
+export class Recent{
+        name:string;
+        mobile:any;
+        email:any;
+        password:any;
+        blood_group:string;
+        landline:string;
+        occupation:string;
+        country:string;
+        state:string;
+        district:string;
+}
